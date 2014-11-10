d67f0711d35b11029f2d6c575d37a1a5a6f7d253
John Napoleone <john.napoleon@gmail.com>
2014-11-10 10:30:50 -0500

    Drupal core update 7.33

--- sites/all/themes/digital_treasures/template.php
+++ sites/all/themes/digital_treasures/template.php
@@ -5,3 +5,13 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Digital Treasures theme.
  */
+
+function napoleone_html_head_alter( &$head_elements ) {
+
+  $head_elements['omega-viewport']['#attributes'] = array (
+    'name'          => 'viewport',
+    'content'       => 'width=device-width',
+    'initial-scale' => '1.0',
+    'minimum-scale' => '1.0',
+  );
+}
