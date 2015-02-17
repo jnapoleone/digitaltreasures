801ee5d63008f6866ca3c4e29349e8ef00fdc9ed
John Napoleone <john.napoleon@gmail.com>
2015-02-17 18:41:26 -0500

    Add Digital Treasures Contact form

--- sites/all/themes/digital_treasures/template.php
+++ sites/all/themes/digital_treasures/template.php
@@ -5,13 +5,3 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Digital Treasures theme.
  */
-
-function digital_treasures_html_head_alter( &$head_elements ) {
-
-  $head_elements['omega-viewport']['#attributes'] = array (
-    'name'          => 'viewport',
-    'content'       => 'width=device-width',
-    'initial-scale' => '1.0',
-    'minimum-scale' => '1.0',
-  );
-}
